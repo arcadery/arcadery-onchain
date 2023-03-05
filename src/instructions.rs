a350bf270d4d08c0ddfba8a9f49e282fed3bf5fe
Burhan Keten <ketenburhan@protonmail.com>
2023-03-05 05:07:53 +0300

    add: essential accounts

--- src/instructions.rs
+++ src/instructions.rs
@@ -1,6 +1,11 @@
 use borsh::BorshDeserialize;
+use shank::ShankInstruction;
 
-#[derive(BorshDeserialize)]
+#[derive(BorshDeserialize, ShankInstruction)]
+#[rustfmt::skip]
 pub enum ArcaderyInstruction {
+    #[account(name = "player", signer, desc = "The player initializing the game")]
+    #[account(name = "game_pda", mut, desc="The game PDA")]
+    #[account(name = "system_program", desc="The system program")]
     InitializeGame,
 }
